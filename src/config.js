da6287c30ccdef7a803008d4e81fcdac3f5277cd
Mustafa Africawala <57094106+mustafa3252@users.noreply.github.com>
2026-02-26 14:24:49 +0000

    Add src/config.js

---
+++ src/config.js
@@ -0,0 +1,48 @@
+export const GAME_W = 1280;
+export const GAME_H = 720;
+
+export const TILE_W = 64;
+export const TILE_H = 32;
+export const WALL_H = 40;
+
+export const CHAR_FRAME_W = 16;
+export const CHAR_FRAME_H = 24;
+export const CHAR_SCALE = 2.5;
+export const CHAR_ANIM_FRAMES = 6;
+
+export const GRID_COLS = 20;
+export const GRID_ROWS = 16;
+
+export const ORIGIN_X = 580;
+export const ORIGIN_Y = 100;
+
+// Tile types
+export const TILE_EMPTY = 0;
+export const TILE_FLOOR = 1;
+export const TILE_WALL = 2;
+export const TILE_DOOR = 3;
+
+// Furniture types
+export const FURN_DESK = 'desk';
+export const FURN_CHAIR = 'chair';
+export const FURN_PLANT = 'plant';
+export const FURN_COOLER = 'cooler';
+export const FURN_WHITEBOARD = 'whiteboard';
+export const FURN_COFFEE = 'coffee';
+export const FURN_BOOKSHELF = 'bookshelf';
+export const FURN_COUCH = 'couch';
+export const FURN_FILING = 'filing';
+
+// Colors
+export const COLORS = {
+    FLOOR_1: { top: 0xc8a882 },
+    FLOOR_2: { top: 0xd0b892 },
+    WALL: { top: 0xe8e0d8, left: 0xc8c0b8, right: 0xb0a8a0 },
+    DOOR: { top: 0x9b7924, left: 0x8a6a18, right: 0x785a10 },
+};
+
+export const SKIN_TONES = [0xfde0c8, 0xf0c8a0, 0xd4a070, 0xc08050, 0x8b5e3c, 0x5c3a1e];
+export const HAIR_COLORS = [0x1a0a00, 0x3a2010, 0x6b4423, 0xdaa520, 0xcc4400, 0x888888, 0xf0e68c];
+export const SHIRT_COLORS = [0x4488cc, 0xcc4444, 0x44aa44, 0x9944cc, 0xddaa22, 0x2288aa, 0x8844aa, 0x44cccc, 0xf08080, 0x6495ed];
+export const PANTS_COLORS = [0x2a2a3a, 0x3a3a4a, 0x4a3a2a, 0x2a3a4a, 0x1a1a2a];
+export const SHOE_COLORS = [0x1a1a1a, 0x3a2a1a, 0x5a3a2a];
